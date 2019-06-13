f826c2dec43f8168b4ff650b90e8b6261b4c1248
jellen44 <34974716+jellen44@users.noreply.github.com>
2019-06-13 16:37:37 -0400

    Update lyrics_mlscript.R

--- lyrics_mlscript.R
+++ lyrics_mlscript.R
@@ -10,6 +10,7 @@ library(SparseM)
 
 #loading in preprocessed data and concatenating to one data frame
 lyricstrain <- read.csv("lyrics_preprocessed1.csv")
+lyricstrain <- lyricstrain[1:4000,]
 #lyrics2 <- read.csv("lyrics_preprocessed2.csv")
 #lyricstrain <- rbind(lyrics1,lyrics2)
 
