f30c180fda973136b34d8eedcb7b72446a1c6ed0
jellen44 <34974716+jellen44@users.noreply.github.com>
2019-06-13 00:02:03 -0400

    Update lyrics_mlscript.R

--- lyrics_mlscript.R
+++ lyrics_mlscript.R
@@ -17,13 +17,14 @@ lyricstrain$lyrics <- as.character(lyricstrain$lyrics)
 traincorpus <- corpus(lyricstrain, text_field = "lyrics")
 
 #creating a document-feature matrix
-traindfm <- dfm(traincorpus,
+dfm.matrixtrain <- dfm(traincorpus,
                 ngrams = 1,
                 remove = stopwords("english"),
                 stem = TRUE,
                 remove_punct = TRUE)
-dfm.matrixtrain <- convert(traindfm, to = "matrix")
+# dfm.matrixtrain <- convert(traindfm, to = "matrix")
 
+#putting examples in a random order
 #putting examples in a random order
 set.seed(nrow(dfm.matrixtrain))
 rand <- sample(nrow(dfm.matrixtrain))
@@ -31,18 +32,39 @@ dfm.matrixtrain <- dfm.matrixtrain[rand,]
 lyricstrain$genre <- lyricstrain$genre[rand]
 
 #creating indices for splitting between training and testing
-end <- dim(dfm.matrixtrain)[1]
-cutoff <- dim(dfm.matrixtrain)[1]*.8
+end <- floor(dim(dfm.matrixtrain)[1])
+cutoff <- floor(dim(dfm.matrixtrain)[1]*.8)
 
 #Training with 80% data split
-ml1 <- caret::train(dfm.matrixtrain[1:cutoff,],
+ml1 <- svm(dfm.matrixtrain[1:cutoff,],
                     lyricstrain$genre[1:cutoff],
-                    method = "rpart")
+           kernel = 'linear', cost=5, probability=TRUE)
 
 #making predictions with 20% data split
-predictions <- predict.train(ml1, newdata=dfm.matrixtrain[cutoff:end,], type="prob")
+preds <- predict(ml1, dfm.matrixtrain[cutoff:end,])
 
 #making and outputting a data frame of predictions
-predictions <- as.data.frame(predictions)
-predictions <- cbind(predictions, lyricstrain$genre[cutoff:end])
-predictions
+accuracy <- function(ypred, y){
+  tab <- table(ypred, y)
+  return(sum(diag(tab))/sum(tab))
+}
+# function to compute precision
+precision <- function(ypred, y){
+  tab <- table(ypred, y)
+  return((tab[2,2])/(tab[2,1]+tab[2,2]))
+}
+# function to compute recall
+recall <- function(ypred, y){
+  tab <- table(ypred, y)
+  return(tab[2,2]/(tab[1,2]+tab[2,2]))
+}
+table(preds, lyricstrain$genre[cutoff:end])
+
+
+accuracy(preds, lyricstrain$genre[cutoff:end])
+precision(preds, lyricstrain$genre[cutoff:end])
+recall(preds, lyricstrain$genre[cutoff:end])
+
+#predictions <- as.data.frame(predictions)
+#predictions <- cbind(predictions, lyricstrain$genre[cutoff:end])
+#predictions
